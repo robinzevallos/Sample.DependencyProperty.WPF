820ca2be3305fbb828963bd6ef30607d28db12ce
robinzevallos <zevallosrobin@gmail.com>
2019-02-05 16:33:10 -0500

    Updated sample for 1.0.3

---
+++ Sample/Traditional Method/DemoControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace Sample.Traditional
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public partial class DemoControl : UserControl
+    {
+        public static readonly DependencyProperty TextProperty
+            = DependencyProperty.Register(
+                "Text",
+                typeof(String),
+                typeof(DemoControl));
+
+        public String Text
+        {
+            get => (String)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextTagProperty 
+            = DependencyProperty.Register(
+                "TextTag",
+                typeof(String), 
+                typeof(DemoControl));
+
+        public String TextTag
+        {
+            get => (String)GetValue(TextTagProperty);
+            set => SetValue(TextTagProperty, value);
+        }
+
+        public static readonly DependencyProperty ForegroundTagProperty
+            = DependencyProperty.Register(
+                "ForegroundTag", 
+                typeof(Brush),
+                typeof(DemoControl));
+
+        public Brush ForegroundTag
+        {
+            get => (Brush)GetValue(ForegroundTagProperty);
+            set => SetValue(ForegroundTagProperty, value);
+        }
+
+        public DemoControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
---
+++ Sample/With Kasay.DependencyProperty/DemoControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Sample
+{
+    using Kasay;
+    using System;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public partial class DemoControl : UserControl
+    {
+        [Bind] public String Text { get; set; }
+
+        [Bind] public String TextTag { get; set; }
+
+        [Bind] public Brush ForegroundTag { get; set; }
+
+        public DemoControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
